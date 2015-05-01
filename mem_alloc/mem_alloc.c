5c9e21c5f1fe55dfe4d319fb96554ecacca551d3
S-D- <daniil.s1001@gmial.com>
2015-05-01 17:04:52 +0300

    Before big changes.

--- mem_alloc/mem_alloc.c
+++ mem_alloc/mem_alloc.c
@@ -14,11 +14,11 @@ struct free_block_header {
 };
 typedef struct free_block_header FreeBH;
 
-struct free_page_header {
-    struct free_page_header* next_ph;
-    char page_area[PAGE_SIZE - sizeof(struct free_page_header*)];
+struct free_page {
+    struct free_page* next_ph;
+    char page_area[PAGE_SIZE - sizeof(struct free_page*)];
 };
-typedef struct free_page_header FreePH;
+typedef struct free_page FreePage;
 
 struct used_page_header {
     struct used_page_header* next_ph;
@@ -28,10 +28,17 @@ struct used_page_header {
 };
 typedef struct used_page_header UsedPH;
 
+union header_ptr {
+    FreePage* free_page;
+    UsedPH* used_ph;
+};
+
 //static void* page_pointers[PAGE_NUM];
 static void* mem_start = NULL;
 static UsedPH* formated_page_lists[BLK_MAX_POW2] = {NULL};
-static FreePH* first_free_page;
+static FreePage* first_free_page;
+static union header_ptr header_ptrs[PAGE_NUM];
+
 
 u_int32_t roundToPow2(u_int32_t v)
 {
@@ -89,21 +96,35 @@ size_t setBit(char* mask, size_t mask_len)
 void* mem_init(void)
 {
     assert(PAGE_SIZE == sysconf(_SC_PAGE_SIZE));
+    /* create mapping */
     void* mapping_area = mmap(NULL, PAGE_NUM*PAGE_SIZE, PROT_READ|PROT_WRITE, 
          MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
     if (mapping_area == MAP_FAILED) {
         return NULL;
     }
-    FreePH* free_phs = mapping_area;
+    /* init list of free pages */
+    FreePage* free_phs = mapping_area;
     for (int i = 1; i < PAGE_NUM; ++i) {
         free_phs[i-1].next_ph = &free_phs[i];
     }
-    free_phs[PAGE_NUM].next_ph = NULL;
+    free_phs[PAGE_NUM-1].next_ph = NULL;
+    first_free_page = free_phs;
+    /* init header_ptrs */
+    for (int i = 0; i < PAGE_NUM; ++i) {
+        header_ptrs[i].free_page = &free_phs[i];
+    }
+    header_ptrs[PAGE_NUM-1].free_page |= 1;
     return mapping_area;
+}
+
+size_t search_free_page(size_t size) {
+    int pages_num = size / PAGE_SIZE;
+    bool b = header_ptrs
 }
 
 void* mem_alloc(size_t size)
 {
+    printf("size: %zu, %zu, %zu\n", sizeof(UsedPH), sizeof(size_t), sizeof(int));
     if (mem_start == NULL) {
         if (mem_init() == NULL) {
             return NULL;
@@ -113,7 +134,7 @@ void* mem_alloc(size_t size)
         uint pow2 = nextPow2(size);
         UsedPH* ph = formated_page_lists[pow2];
         size_t block_len = 1;
-        block_len = block_len << pow2; //in bits
+        block_len <<= pow2; //in bits ??
         size_t mask_len = PAGE_SIZE / block_len / 8;
         if (ph != NULL) {
             ph->blocks_free--;
@@ -129,13 +150,13 @@ void* mem_alloc(size_t size)
             return NULL; // no memory left
         }
         block_len /= 8; // in bytes
-        FreePH* fp = first_free_page;
+        FreePage* fp = first_free_page;
         first_free_page = fp->next_ph;
         UsedPH* new_ph;
-        if (pow2 == 4) {
+        if (pow2 == 5) { // 4?
             new_ph = (UsedPH*)fp;
             new_ph->blocks_free = PAGE_SIZE / block_len - 1;
-            new_ph->first_block = (char*) new_ph + sizeof(UsedPH)+16;
+            new_ph->first_block = (char*) new_ph + sizeof(UsedPH)+mask_len;
         } else {
             new_ph = mem_alloc(sizeof(UsedPH) + mask_len);
             new_ph->blocks_free = PAGE_SIZE / block_len;
@@ -146,7 +167,18 @@ void* mem_alloc(size_t size)
         new_ph->usage_mask[0] = 1;
         return new_ph->first_block;
     }
-    printf("size: %zu, %zu, %zu\n", sizeof(UsedPH), sizeof(size_t), sizeof(int));
+    
     return NULL;
 }
+
+void* mem_realloc(void *addr, size_t size)
+{
+    
+}
+
+void mem_free(void *addr)
+{
+    
+}
+
 //TODO check pointers cast for UB
